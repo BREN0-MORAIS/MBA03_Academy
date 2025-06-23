2d877e1caa61181bdc767e03ecb08f4b08e08548
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2025-06-23 14:21:04 -0400

    feature: configuração de campos

--- Academy/src/GestaoConteudo/Academy.GestaoConteudo.Domain/ObjectValue/ConteudoProgramatico.cs
+++ Academy/src/GestaoConteudo/Academy.GestaoConteudo.Domain/ObjectValue/ConteudoProgramatico.cs
@@ -3,5 +3,17 @@
 public class ConteudoProgramatico
 {
     public string Objetivo { get; private set; }
-    public string PreRequisitos { get; private set; }   
+    public string PreRequisitos { get; private set; }
+
+    public ConteudoProgramatico(string objetivo, string preRequisitos)
+    {
+        Objetivo = objetivo;
+        PreRequisitos = preRequisitos;
+        Validar();
+    }
+
+    public void Validar()
+    {
+
+    }
 }
